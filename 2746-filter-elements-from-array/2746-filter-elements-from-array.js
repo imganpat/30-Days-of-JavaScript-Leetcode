95b1a92fcb8eb12a08e0b364dd34647be4ac06e8
Ganpat <coolganpat41@gmail.com>
2025-04-01 19:53:09 +0530

    Time: 51 ms (29.38%), Space: 54 MB (19%) - LeetHub

---
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let filteredArr = [];
+    const length = arr.length;
+    for(let i = 0; i < length; i++)
+        if(fn(arr[i], i)) 
+            filteredArr.push(arr[i]);
+        
+    return filteredArr;
+};
