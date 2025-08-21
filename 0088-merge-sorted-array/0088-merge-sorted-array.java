e5004110422f336fcb3427c4973f9d84485dae4a
Ganpat <coolganpat41@gmail.com>
2025-08-21 20:31:29 +0530

    Time: 0 ms (100%), Space: 42.6 MB (15.57%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,16 +1,31 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for(int i = m ; i<m+n; i++)
-            nums1[i] = nums2[i-m];
-
-        for(int i = 0; i < nums1.length; i++) {
-            for(int j = i+1; j < nums1.length; j++) {
-                if(nums1[i] > nums1[j]) {
-                    int temp = nums1[i];
-                    nums1[i] = nums1[j];
-                    nums1[j] = temp;
-                }
-            }
+        int a = m-1, b = n-1, c = m+n-1;
+        while(a >= 0 && b >= 0) {
+            if(nums2[b] > nums1[a]) 
+                nums1[c--] = nums2[b--];
+            else 
+                nums1[c--] = nums1[a--];
         }
+
+        while (b >= 0) 
+            nums1[c--] = nums2[b--];
+         
+        // another solution 
+        // copy all elements form nums2 to nums1
+        // for(int i = m ; i<m+n; i++)
+        //     nums1[i] = nums2[i-m];
+
+        // use any sorting alogrithm
+        // i have used bubble sort
+        // for(int i = 0; i < nums1.length; i++) {
+        //     for(int j = i+1; j < nums1.length; j++) {
+        //         if(nums1[i] > nums1[j]) {
+        //             int temp = nums1[i];
+        //             nums1[i] = nums1[j];
+        //             nums1[j] = temp;
+        //         }
+        //     }
+        // }
     }
 }
