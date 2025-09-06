2b35216d625f982754db913f08e58ba9541b9cb6
Ganpat <coolganpat41@gmail.com>
2025-09-06 21:33:45 +0530

    Time: 2 ms (7.73%), Space: 45.6 MB (15.41%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.java
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.java
@@ -3,8 +3,9 @@ class Solution {
         int length = nums.length;
         int [] ans = new int[length];
         for(int i = 0; i < length; i++){
-            ans[i] = nums[nums[i]];
-        } 
+            ans[i] = nums[i] + nums[nums[i]] * length;
+            ans[i] = nums[nums[i]] % length;
+        }
         return ans;
     }
 }
