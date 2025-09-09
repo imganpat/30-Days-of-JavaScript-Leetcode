7399eedf188bbc362b7a7b173bee98d8a638d1eb
Ganpat <coolganpat41@gmail.com>
2025-09-09 21:07:52 +0530

    Time: 0 ms (100%), Space: 41.5 MB (75.67%) - LeetHub

---
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.java
@@ -0,0 +1,18 @@
+// Approach: Brute Force
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+
+class Solution {
+    public String defangIPaddr(String address) {
+        StringBuilder sb = new StringBuilder();
+        int addressLength = address.length();
+        for(int i = 0; i < addressLength; i++){
+            if(address.charAt(i) == '.'){
+                sb.append("[.]");
+            } else {
+                sb.append(address.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
