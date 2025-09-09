45918752ddade9edbe35582c3fff5cf132e58efd
Ganpat <coolganpat41@gmail.com>
2025-09-09 21:14:13 +0530

    Time: 0 ms (100%), Space: 41.3 MB (88.53%) - LeetHub

--- 1205-defanging-an-ip-address/1205-defanging-an-ip-address.java
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.java
@@ -4,6 +4,18 @@
 
 class Solution {
     public String defangIPaddr(String address) {
-        return address.replace(".", "[.]");
+        StringBuilder sb = new StringBuilder();
+        int addressLength = address.length();
+        for(int i = 0; i < addressLength; i++){
+            if(address.charAt(i) == '.'){
+                sb.append("[.]");
+            } else {
+                sb.append(address.charAt(i));
+            }
+        }
+        return sb.toString();
+
+        // using in built function
+        // return address.replace(".", "[.]");
     }
 }
