e5482f049489c1545eaf2498a34be076d9c11754
Ganpat <coolganpat41@gmail.com>
2025-09-17 21:11:15 +0530

    Time: 2 ms (92.81%), Space: 42.6 MB (75%) - LeetHub

--- 3567-convert-date-to-binary/3567-convert-date-to-binary.java
+++ 3567-convert-date-to-binary/3567-convert-date-to-binary.java
@@ -7,7 +7,8 @@ class Solution {
         int year = Integer.parseInt(date.substring(0,4));
         int month = Integer.parseInt(date.substring(5,7));
         int d = Integer.parseInt(date.substring(8,10));
-        StringBuilder res = new StringBuilder(Integer.toBinaryString(year));
+        StringBuilder res = new StringBuilder();
+        res.append(Integer.toBinaryString(year));
         res.append("-");
         res.append(Integer.toBinaryString(month));
         res.append("-");
