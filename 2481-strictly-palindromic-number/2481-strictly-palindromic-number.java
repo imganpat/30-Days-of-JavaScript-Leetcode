2a86e6fa3797fe517c790e2ba0d201ed7b772a8a
Ganpat <coolganpat41@gmail.com>
2025-10-27 22:38:58 +0530

    Time: 0 ms (100%), Space: 40.5 MB (42.09%) - LeetHub

---
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isStrictlyPalindromic(int n) {
+        //     for(int i = 2; i < n - 1; i++){
+        //         System.out.println(i);
+
+        //     }
+        //     System.out.println(convert(n, 2));
+        //     return false;
+        // }
+        // public int convert(int n, int base){
+        //     int res = 0;
+        //     int t = n;
+        //     while(n < 0){
+        //         res = res * 10 + (t % base);
+        //         t/=base;
+        //     }
+        //     return res;
+
+        return false;
+    }
+}
