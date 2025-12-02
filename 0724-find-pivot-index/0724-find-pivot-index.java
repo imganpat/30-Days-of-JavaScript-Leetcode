42db8e49f23d3dff6df5318d071f22a97cb1799f
Ganpat <coolganpat41@gmail.com>
2025-12-02 18:27:14 +0530

    Time: 1 ms (94.74%), Space: 47.3 MB (24.69%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -4,13 +4,11 @@ class Solution {
         int[] leftSum = new int[n], rightSum = new int[n];
 
         leftSum[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            leftSum[i] = leftSum[i - 1] + nums[i];
-        }
-
         rightSum[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            rightSum[i] = rightSum[i + 1] + nums[i];
+
+        for (int i = 1, j = n - 2; i < n; i++, j--) {
+            leftSum[i] = leftSum[i - 1] + nums[i];
+            rightSum[j] = rightSum[j + 1] + nums[j];
         }
 
         for (int i = 0; i < n; i++) {
